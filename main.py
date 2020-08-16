0842286cbc8f96344dcd4ef8b3709a2dbc79e16b
Mudassir Khan <just00seven@gmail.com>
2020-08-16 14:00:01 +0000

    function added

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import turtle
 
-a = turtle.Turtle()
-a.forward(200)
-a.right(90)
-a.forward(200)
-a.right(90)
-a.forward(200)
-a.right(90)
-a.forward(200)
 
+a = turtle.Turtle()
+
+def square():
+  a.forward(200)
+  a.right(90)
+  a.forward(200)
+  a.right(90)
+  a.forward(200)
+  a.right(90)
+  a.forward(200)
+
+square()
+a.forward(200)
+square()

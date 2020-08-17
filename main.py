ea0a803b240a03e2ae163efd754bc913e5330e11
Mudassir Khan <just00seven@gmail.com>
2020-08-17 06:07:32 +0000

    if else statement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import turtle
-
-
 a = turtle.Turtle()
 
 def square():
@@ -12,6 +10,38 @@ def square():
   a.right(90)
   a.forward(200)
 
-square()
-a.forward(200)
-square()
+# square()
+# a.forward(200)
+# square()
+
+# import calendar
+# print(calendar.weekheader(3))
+
+# print(calendar.firstweekday())
+# print()
+
+# print(calendar.month(1980, 12 , w=2))
+# print()
+
+# print(calendar.monthcalendar(2019,5))
+# print()
+
+# print(calendar.calendar(2020))
+
+# a = calendar.weekday(2020 , 7 , 13)
+# print(a)
+
+# leap = calendar.isleap(2020)
+# print(leap)
+
+dino = 1000
+elephant = 100
+if(dino < elephant):
+  square()
+else:
+  a.forward(200)
+
+
+
+# if True:
+#   square()
